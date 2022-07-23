9fb6dac71f5dd28d6110834f45845f19dfc253d5
Robiianna <robidelaparra1@gmail.com>
2022-07-23 23:31:45 +0000

    finalizado

--- src/models.py
+++ src/models.py
@@ -16,10 +16,27 @@ class User(Base):
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
 
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Planet_Favorite(Base):
+    __tablename__ = 'planet_favorite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+
+    def to_dict(self):
+        return {}
+
 class Character(Base):
     __tablename__ = 'character'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
@@ -36,25 +53,7 @@ class Character_Favorite(Base):
     def to_dict(self):
         return {}
 
-class Planet(Base):
-    __tablename__ = 'planet'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Planet_Favorite(Base):
-    __tablename__ = 'planet_favorite'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-    planet = relationship(Planet)
-
-    def to_dict(self):
-        return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
